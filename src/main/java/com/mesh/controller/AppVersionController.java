1b3d571ea05f9e1429e0ecb2d8d5a0b2b3be9ab0
Swapnil <swapnil.ajabe1212@gmail.com>
2026-09-22 12:07:13 +0530

    Added Update feature latest app version 1.0.0

---
+++ src/main/java/com/mesh/controller/AppVersionController.java
@@ -0,0 +1,23 @@
+package com.mesh.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/app")
+public class AppVersionController {
+
+    @GetMapping("/version")
+    public ResponseEntity<Map<String, Object>> getLatestVersion() {
+        return ResponseEntity.ok(Map.of(
+                "latestVersionCode", 2,
+                "latestVersionName", "1.1.0",
+                "downloadUrl", "https://meshpay-backend-latest.onrender.com/download/MeshPay.apk", // Or Google Drive/GitHub link
+                "forceUpdate", false
+        ));
+    }
+}
